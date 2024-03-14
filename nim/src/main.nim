648c069d28d22026fe266c3a1123e440d80d14df
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-14 13:19:47 -0400

    Update nim todo list to delete with proper id numbers

--- nim/src/main.nim
+++ nim/src/main.nim
@@ -7,21 +7,22 @@ proc greet() =
     echo "To get started, please type a command or `help`.\n"
 
 var 
-    current_id = 0
-    tasks: seq[tuple[id: int, body: string]]
+    tasks: seq[string]
 
 proc delete_task(id: int) = 
-    let new_tasks = tasks.filter(x => x.id != id-1)
-    tasks = new_tasks
+    var new_list: seq[string]
+    for i, elem in tasks:
+        if i != id-1:
+            new_list.add(elem)
+    tasks = new_list
 
 proc add_task(task: string) = 
-    tasks.add((current_id, task))
-    current_id += 1
+    tasks.add(task)
 
 proc display_todo_list() = 
     echo "Todo List:\n"
     for i, elem in tasks:
-        echo &"{i+1}.) {elem.body}"
+        echo &"{i+1}.) {elem}"
     echo "\n"
 
 when isMainModule:
