7e54baa84ddad0378d5214c1cc41456bf404e00c
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-13 21:51:31 -0400

    Remove js compilation from git tracking

---
+++ rust/target/debug/deps/rust-3e30ec0536239abc.d
@@ -0,0 +1,5 @@
+/Users/benlandrette/ccode/multi-language-practice/small-practice-projects/todo-list/rust/target/debug/deps/librust-3e30ec0536239abc.rmeta: src/main.rs
+
+/Users/benlandrette/ccode/multi-language-practice/small-practice-projects/todo-list/rust/target/debug/deps/rust-3e30ec0536239abc.d: src/main.rs
+
+src/main.rs:
